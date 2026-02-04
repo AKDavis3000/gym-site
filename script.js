b492a2e217aa30332952cba213fe75358a93ce9a
AKDavis95 <112223134+AKDavis95@users.noreply.github.com>
2026-02-03 21:23:49 -0500

    final commit

--- script.js
+++ script.js
@@ -6,7 +6,8 @@ const navMenu = document.querySelector('nav');
 
 // open nav with click of bars
 const openNavMenu = () => {
-  navMenu.style.right = '0px';
+  navMenu.style.display = 'block';
+
   // removes scroll while nav is open
   document.body.style.overflow = 'hidden';
 };
@@ -14,9 +15,24 @@ navOpen.addEventListener('click', openNavMenu);
 
 // close nav with x mark
 const closeNavMenu = () => {
-  navMenu.style.right = '800px';
+  navMenu.style.display = 'none';
   // adds scroll while nav is closed
-
   document.body.style.overflow = 'visible';
 };
 navClose.addEventListener('click', closeNavMenu);
+
+// highlight active nav link
+const activePage = window.location.pathname;
+const navLinks = document.querySelectorAll('nav a');
+navLinks.forEach((link) => {
+  if (link.href.includes(`${activePage}`)) {
+    link.classList.add('active');
+  }
+});
+
+// loader
+const loaderContainer = document.querySelector('.loader-container');
+
+window.addEventListener('load', () => {
+  loaderContainer.classList.add('hidden');
+});
